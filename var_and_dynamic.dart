45f69c779f12f87ebdf407d00cd2653d99ea55c8
Het <hetpatel2808hp@gmail.com>
2022-12-21 13:35:54 +0530

    use of dynamic variables using VAR keyword

---
+++ var_and_dynamic.dart
@@ -0,0 +1,22 @@
+void main() {
+  print("Welcome to dart");
+
+  String name = "Het Patel";
+  print(name);
+
+  //var is a keyword which itself identifies it's variable types.
+  var subject = "Maths";
+  print(subject);
+
+  var roolno = 47;
+  print(roolno);
+
+  //intializing the var without inline so that it can store anything.
+  var section;
+  //it can store multiple kind of data and can be changed dynamically
+  section = "Yogesh Saini";
+  // section = 22;
+  //section = false;
+  //section = 492.02;
+  print(section);
+}
